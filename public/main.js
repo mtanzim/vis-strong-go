03a4ed8584e86a5d71874eb548ea2dadaa6cb1af
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-05-20 00:02:58 -0400

    remove useless fn

--- public/main.js
+++ public/main.js
@@ -30,7 +30,3 @@ async function main() {
 }
 
 window.strongMain = main;
-window.strongBackToTop = function () {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-};
