68d66ebbf8e859db6e51a645eabdf7ae465c9c86
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-05-24 12:22:49 -0400

    something that works!

--- client/preact.config.js
+++ client/preact.config.js
@@ -2,6 +2,11 @@ import { DefinePlugin } from "webpack";
 
 export default {
   webpack(config) {
+    config.externals = {
+      externals: {
+        Plotly: "Plotly",
+      },
+    };
     config.plugins.push(
       new DefinePlugin({
         __BASE_API__: JSON.stringify(process.env.BASE_API || ""),
